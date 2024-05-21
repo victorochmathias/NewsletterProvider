2bf57b071e37e09942f91c52edc8dcf876d5383f
victor <myran_virre@hotmail.com>
2024-05-21 14:55:49 +0200

    Nu fungerar det!!

--- NewsletterProvider1/Program.cs
+++ NewsletterProvider1/Program.cs
@@ -1,17 +1,24 @@
 using Data.Contexts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureAppConfiguration((context, config) =>
     {
-
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
+        var connectionString = configuration.GetConnectionString("SqlServer");
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(connectionString));
     })
     .Build();
 
