44ce7525b0499235cfcd5813b5c50a948ede2d8e
victor <myran_virre@hotmail.com>
2024-05-21 13:57:36 +0200

    Ändrat lite i program.cs

--- NewsletterProvider1/Program.cs
+++ NewsletterProvider1/Program.cs
@@ -1,17 +1,25 @@
 using Data.Contexts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((context, config) =>
     {
-        
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
+
+        // Hämta anslutningssträngen från konfigurationen
+        var connectionString = context.Configuration.GetConnectionString("SqlServer");
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(connectionString));
     })
     .Build();
 
